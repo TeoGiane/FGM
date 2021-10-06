354844ac2368ec20707171db536c11dad44cd458
alessandrocolombi <58763854+alessandrocolombi@users.noreply.github.com>
2021-10-06 14:24:46 +0200

    New R files to include BDgraph code

---
+++ R/detect_cores.R
@@ -0,0 +1,79 @@
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+#     Copyright (C) 2012 - 2019  Reza Mohammadi                                |
+#                                                                              |
+#     This file is part of BDgraph package.                                    |
+#                                                                              |
+#     BDgraph is free software: you can redistribute it and/or modify it under |
+#     the terms of the GNU General Public License as published by the Free     |
+#     Software Foundation; see <https://cran.r-project.org/web/licenses/GPL-3>.|
+#                                                                              |
+#     Maintainer: Reza Mohammadi <a.mohammadi@uva.nl>                          |
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+
+get_cores = function( cores = NULL )
+{
+    num_machine_cores = detect_cores()
+    if( is.null( cores ) ) cores = num_machine_cores - 1
+    if( cores == "all" )   cores = num_machine_cores
+    .C( "omp_set_num_cores", as.integer( cores ), PACKAGE = "FGM" )
+    return( cores )
+}
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+detect_cores = function( all.tests = FALSE, logical = TRUE )
+{
+	if( .Platform $ OS.type == "windows" )
+	{
+		if( logical )
+		{
+			res <- Sys.getenv( "NUMBER_OF_PROCESSORS", "1" )
+			as.numeric( res )
+		}else{
+			x = system( "WMIC CPU Get DeviceID,NumberOfCores", intern = TRUE )
+			sum( utils::read.table( text = x, header = TRUE ) $ NumberOfCores )
+		}
+	}else{
+		systems = list(
+
+			linux =
+				if( logical )
+					"grep processor /proc/cpuinfo 2>/dev/null | wc -l"
+				else
+					"cat /proc/cpuinfo | grep 'cpu cores'| uniq | cut -f2 -d:",
+
+			darwin =
+				if( logical )
+					"/usr/sbin/sysctl -n hw.logicalcpu 2>/dev/null"
+				else
+					"/usr/sbin/sysctl -n hw.physicalcpu 2>/dev/null",
+
+			solaris =
+				if( logical )
+					"/usr/sbin/psrinfo -v | grep 'Status of.*processor' | wc -l"
+				else
+					"/bin/kstat -p -m cpu_info | grep :core_id | cut -f2 | uniq | wc -l",
+
+			freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
+
+			openbsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
+
+			irix = c( "hinv | grep Processors | sed 's: .*::'", "hinv | grep '^Processor '| wc -l" )
+		)
+
+		for( i in seq( systems ) )
+		{
+			if( all.tests || length( grep( paste0( "^", names( systems )[i] ), R.version $ os ) ) )
+				for( cmd in systems[i] )
+				{
+					a = try( suppressWarnings( system( cmd, TRUE ) ), silent = TRUE )
+					if( inherits( a, "try-error" ) ) next
+					a <- gsub( "^ +", "", a[1] )
+					if( length( grep( "^[1-9]", a ) ) ) return( as.integer( a ) )
+				}
+		}
+
+		NA_integer_
+	}
+}
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
---
+++ R/rmvnorm.R
@@ -0,0 +1,37 @@
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+#     Copyright (C) 2012 - 2019  Reza Mohammadi                                |
+#                                                                              |
+#     This file is part of BDgraph package.                                    |
+#                                                                              |
+#     BDgraph is free software: you can redistribute it and/or modify it under |
+#     the terms of the GNU General Public License as published by the Free     |
+#     Software Foundation; see <https://cran.r-project.org/web/licenses/GPL-3>.|
+#                                                                              |
+#     Maintainer: Reza Mohammadi <a.mohammadi@uva.nl>                          |
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+#     Data generator from multivarate normal distribution                      |
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+
+#' BDgraph sampling from multivariate normal
+#'
+#' @export
+rmvnorm = function( n = 10, mean = rep( 0, length = ncol( sigma ) ), sigma = diag( length( mean ) ) )
+{
+    if( !isSymmetric( sigma, tol = sqrt( .Machine$double.eps ), check.attributes = FALSE ) )
+        stop( "sigma must be a symmetric matrix" )
+
+    sigma <- as.matrix( sigma )
+    p     <- nrow( sigma )
+    if( length( mean ) == 1 ) mean <- rep( mean, p )
+    if( length( mean ) != nrow( sigma ) ) stop( "mean and sigma have non-conforming size" )
+
+    # - - generate multivariate normal data - - - - - - - - - - - - - - - - - -|
+    chol_sig <- chol( sigma )
+    z        <- matrix( stats::rnorm( p * n ), p, n )
+    data     <- t( chol_sig ) %*% z + mean
+    data     <- t( data )
+
+    return( data )
+}
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
